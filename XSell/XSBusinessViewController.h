8d65eb85228152bb2b3b4a151efa5f2dd097236b
Fahad Ahmed <fahadahmed@Fahads-MacBook-Pro.local>
2014-05-19 12:25:58 +1000

    Created Business Connect Page and Business Pages

---
+++ XSell/XSBusinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  XSBusinessViewController.h
+//  XSell
+//
+//  Created by Fahad Ahmed on 19/05/2014.
+//  Copyright (c) 2014 GE Capital ANZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XSBusinessViewController : UIViewController
+
+@end
